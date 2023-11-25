07906ec705f09f01a2a7aef3ba81837e7e8777a6
Gabriel R Talamini <1135583@atitus.edu.br>
2023-11-25 13:02:56 -0300

    adicionado código para criar o executavel

---
+++ setup.py
@@ -0,0 +1,17 @@
+import cx_Freeze
+executables = [
+    cx_Freeze.Executable(script="main.py", icon="icospace.ico")
+]
+cx_Freeze.setup(
+    name = "SpaceMarker",
+    options = {
+        "build_exe":{
+            "packages": ["pygame"],
+            "include_files": [
+                "fundospace.jpg",
+                "space.png",
+                "Space_Machine_Power.mp3"
+            ]
+        }
+    } , executables = executables
+)
